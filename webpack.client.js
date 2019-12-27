4babb6f04c5d06d5b384250563726083d9bb043b
7ma7X <hellorusk1998@gmail.com>
2019-12-27 23:01:38 +0900

    fix css

--- webpack.client.js
+++ webpack.client.js
@@ -13,7 +13,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       meta: [
-        {viewport: "width=device-width, initial-scale=1.0"},
+        {name: "viewport", content: "width=device-width, initial-scale=1.0"},
         {"http-equiv": "X-UA-Compatible", content: "IE=edge"},
         {"charset": "UTF-8"}
       ]
