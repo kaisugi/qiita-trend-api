f7b4ed31c71ca9853a4d236109a6bf51e84b42b3
7ma7X <hellorusk1998@gmail.com>
2019-12-27 21:16:21 +0900

    create entry point

---
+++ src/lambda/trend.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+const fetchTrend = (html: string) => {
+  const $ = cheerio.load(html)
+  const raw = $('div[data-hyperapp-app="Trend"]').attr('data-hyperapp-props')
+  if (!raw) {
+    return {}
+  } else {
+    JSON.parse(raw).trend.edges
+  }
+}
+
+export const handler = async () => {
+  const url = 'https://qiita.com/'
+
+  return await axios
+    .get(url)
+    .then(({ data }) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(fetchTrend(data)),
+      }
+    })
+    .catch(err => {
+      return {
+        statusCode: 500,
+        body: err,
+      }
+    })
+}
