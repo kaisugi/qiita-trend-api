dcd2a06df690105a1276fd97e3b698e3f8494947
Kaito Sugimoto <7X.rusk@gmail.com>
2019-12-28 18:04:18 +0900

    fix Content-Type

--- src/lambda/trend.ts
+++ src/lambda/trend.ts
@@ -18,6 +18,7 @@ export const handler = async () => {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(fetchTrend(data)),
       }
