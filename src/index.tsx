c941658f936029a320aa903b95b163e2bf062638
7ma7X <hellorusk1998@gmail.com>
2019-12-31 23:13:23 +0900

    fix design

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ const Main = () => {
       <h1 className={styles.h1}>Qiita Trend API</h1>
       <div>
         <p>Qiita のトレンド記事の一覧を JSON で返す非公式 API です。</p>
-        <p><a href="/.netlify/functions/trend">トレンド記事一覧（JSON）</a></p>
-        <p><Link to="/trend">トレンド記事一覧（テキスト）</Link></p>
+        <p><a href="/.netlify/functions/trend">トレンド記事一覧（JSON）</a><br/>
+        <Link to="/trend">トレンド記事一覧（テキスト）</Link></p>
         <p>Qiita 側の仕様変更などにより、予告なくサービスを終了する場合があります。<br/>
         何かありましたら <a href="https://github.com/7ma7X/qiita-trend-api/issues">Issue</a> にてお願いいたします。</p>
         <p>@ 2019 HelloRusk</p>
